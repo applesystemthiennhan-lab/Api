9b48caab432553b1a31e388b3571c3cdad31145e
applesystemthiennhan-lab <applesystemthiennhan@gmail.com>
2025-11-01 20:39:33 +0700

    Create key.js

---
+++ key.js
@@ -0,0 +1,18 @@
+=== NỘI DUNG key.js (COPY PHẦN NÀY VÀ UP LÊN GITHUB RAW) ===
+
+// key.js (public raw) - chứa HASH, không chứa key gốc
+const API_KEY_HASH = 'pbkdf2-sha256$150000$8f3a2b1c4d5e6f7a8b9c0d1e2f3a4b5c$e1d2c3b4a5...';
+const DEVICE_KEY_HASH = 'pbkdf2-sha256$150000$a1b2c3d4e5f60718293a4b5c6d7e8f90$b1c2d3e4f5...';
+const EXPIRES = '2026-10-31T12:34:56Z';
+
+// Bạn có thể export object nếu muốn:
+// export default { API_KEY_HASH, DEVICE_KEY_HASH, EXPIRES };
+
+=== KEY NGUYÊN BẢN (LƯU CẨN THẬN, KHÔNG UP LÊN GITHUB) ===
+
+APIKEY: apikey-3f9a7b8c4d1e2f3aa4b5c6
+DEVICEKEY: DeviceAPI-9b8c7d6e5f4a3b2c1d0e
+
+=== GHI CHÚ ===
+- iterations (PBKDF2): 150000
+- EXPIRES (ISO UTC): 2026-10-31T12:34:56Z
